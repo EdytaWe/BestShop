48ad4de22f5b07736c4826c19072f3e5922e26c9
EdytaWE <edytawesolowska@gmail.com>
2022-01-30 21:16:37 +0100

    add functional constructor named Calculator

--- gulpfile.js
+++ gulpfile.js
@@ -27,7 +27,7 @@ function watcher(done) {
     gulp.watch(entryPath + "/**/*.scss", gulp.series(compileSass, reload));
     gulp.watch("./public/*.html", gulp.series(reload));
 
-    gulp.watch(entryPath + "/js/*.js", gulp.series(reload));
+    gulp.watch("./public/js/*.js", gulp.series(reload));
 
     done();
 }
